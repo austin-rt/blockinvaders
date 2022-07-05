ea06de9d3b00f5d8f5750a39878c267f55783246
Austin Taylor <austinrtaylor91@gmail.com>
2022-07-05 09:13:47 -0400

    added shoot timer

--- js/script.js
+++ js/script.js
@@ -1,20 +1,21 @@
 const board = document.getElementById('board');
 let playerPosition = 389;
+let canPlayerShoot = true;
+let shootTimer = 1000;
+
 for (let i = 0; i < 400; i++) {
   const cell = document.createElement('div');
   board.appendChild(cell);
   cell.classList.add('cell');
   cell.innerText = i;
 }
-
-const cells = Array.from(document.querySelectorAll('.cell'));
+const cells = [...board.children];
 
 const invaders = [
-  5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
-  25, 26, 27, 28, 29, 30, 31, 32, 33, 34,
-  45, 46, 47, 48, 49, 50, 51, 52, 53, 54,
-  65, 66, 67, 68, 69, 70, 71, 72, 73, 74,
-  85, 86, 87, 88, 89, 90, 91, 92, 93, 94
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+  20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+  40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
+  60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79,
 ];
 
 const fillSquares = () => {
@@ -57,21 +58,32 @@ const moveRight = () => {
     placePlayer();
   }
 };
+
 const shoot = () => {
-  let bulletPosition = playerPosition;
-  const moveBullet = () => {
-    if (bulletPosition > 19) {
-      cells[bulletPosition].classList.remove('bullet');
-      bulletPosition -= 20;
-      cells[bulletPosition].classList.add('bullet');
-      if (cells[bulletPosition].classList.contains('invader')) {
-        cells[bulletPosition].classList.remove('invader');
+  if (canPlayerShoot === true) {
+    let bulletPosition = playerPosition;
+    const moveBullet = () => {
+      if (bulletPosition > 19) {
         cells[bulletPosition].classList.remove('bullet');
-        clearInterval(bulletTimer);
+        bulletPosition -= 20;
+        cells[bulletPosition].classList.add('bullet');
+        if (cells[bulletPosition].classList.contains('invader')) {
+          cells[bulletPosition].classList.remove('invader');
+          cells[bulletPosition].classList.remove('bullet');
+          clearInterval(bulletTimer);
+        }
+      } else {
+        cells[bulletPosition].classList.remove('bullet');
       }
-    } else {
-      cells[bulletPosition].classList.remove('bullet');
-    }
-  };
-  bulletTimer = setInterval(moveBullet, 50);
+    };
+    canPlayerShoot = false;
+    bulletTimer = setInterval(moveBullet, 50);
+    setTimeout(playerCanShoot, shootTimer);
+    console.log(canPlayerShoot);
+  }
+};
+
+const playerCanShoot = () => {
+  canPlayerShoot = true;
+  console.log(canPlayerShoot);
 };
